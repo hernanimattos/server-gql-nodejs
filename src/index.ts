6df18f6431c91764ac5d6decbab1b358a05e64b9
Hernani Luiz de mattos <hernani.luiz@gmail.com>
2022-06-06 19:39:03 -0300

    ttest

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { importSchema } from 'graphql-import';
 import { resolvers } from './graphQL';
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 app.use(express.json());
 
 app.use(cors());
